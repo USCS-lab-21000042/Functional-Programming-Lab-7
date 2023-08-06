a50b04bedd8355358cbcbaa900a3531108dedf16
visal <visaluni2@gmail.com>
2023-08-06 22:45:41 +0530

    Did modifications

--- Q1.scala
+++ Q1.scala
@@ -1,11 +1,13 @@
 import scala.io.StdIn
 object test extends App{
-    def filterEvenNumbers()={
+    def filterEvenNumbers():List[Int]={
         print("Enter integers :")
         var line=StdIn.readLine()
         var numbers=line.split(" ").map(_.toInt).toList
-        val num=(x:Int)=> if x%2==0 then true else false
-        var list=numbers.filter(num)
-        print(s"Even number ${list}")
+        //val num=(x:Int)=> if x%2==0 then true else false
+        var list=numbers.filter(l=>l%2==0)
+        //print(s"Even numbers ${list}")
+        return list
     }
+    print(s"Even numbers ${filterEvenNumbers()}")
 }
